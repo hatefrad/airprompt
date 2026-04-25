4f1321c63880562cab2341635cef0cf61b2d6d10
hatefrad <hatefrad@gmail.com>
2026-04-25 16:28:55 +0200

    fix: forward argv to child process in bin shim

--- bin/airprompt.js
+++ bin/airprompt.js
@@ -6,7 +6,7 @@ import { dirname, join } from 'path'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const script = join(__dirname, '..', 'dist', 'src', 'index.js')
 
-const child = spawn(process.execPath, [script], { stdio: 'inherit' })
+const child = spawn(process.execPath, [script, ...process.argv.slice(2)], { stdio: 'inherit' })
 
 child.on('exit', (code, signal) => {
   if (signal) {
